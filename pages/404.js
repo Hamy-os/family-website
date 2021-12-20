a9add4ea6d13e6e2dbbd32772dd65adf9ad5afd3
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-12-20 13:48:24 +0300

    feat(error handling): 500 error page and 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+  const NotFound = () => {
+    return (
+      <div>
+        {/*TODO: Actually put something here 😋*/}
+        404 page!
+      </div>
+    );
+  }
+   
+  export default NotFound;
