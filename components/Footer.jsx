bbfe9824314d4723129f4127487aff1fdf6cff7b
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-12-23 18:20:15 +0300

    tweak(footer): small design tweaks

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 const Footer = () => {
   return (
   <div>
-    <footer class="mb-2 footer footer-center">
+    <footer className="mb-2 footer footer-center">
       <div>
-        <p>Made with ❤ by <a className="underline decoration-indigo-500 decoration-wavy decoration-2" href="https://github.com/Hamy-os/">Hamza</a> - Copyright © 2021 - All right reserved</p>
+        <p>Made with ❤ by <a className="underline decoration-indigo-500 decoration-wavy decoration-2 sm:text-sm p-2" href="https://github.com/Hamy-os/">Hamza</a> - Copyright © 2021 - All right reserved</p>
       </div> 
     </footer>
   </div>
