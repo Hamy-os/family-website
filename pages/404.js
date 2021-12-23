209b15837d51346e1a6b562f318467f64f9f1f20
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-12-23 18:17:46 +0300

    tweak(error/404) Simple but cute 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,16 @@
-  const NotFound = () => {
+import Link from 'next/link';
+
+const NotFound = () => {
     return (
-      <div>
-        {/*TODO: Actually put something here 😋*/}
-        404 page!
+      <div className="text-center ">
+        <h1 className="text-4xl md:text-8xl py-8">404 / Not Found!</h1>
+        <p className="text-2xl md:text-4xl py-8">You Took A wrong Turn Or Followed A Broken Link!</p>
+          <Link href="/">
+            <div className="btn">
+              Go Home
+            </div>
+          </Link>
       </div>
     );
   }
-   
   export default NotFound;
