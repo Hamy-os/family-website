ce53a0868e0cb60aadad7381d67dc1aa1736ca6e
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-12-23 18:22:48 +0300

    tweak(config): daisyui config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,46 @@
 const colors = require('tailwindcss/colors')
-
 module.exports = {
+  variants: {
+    extend: {
+      visibility: ["group-hover"],
+    },
+   },
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.neutral,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
-      bluefooter: {
-        light: '#1C3E7E',
-        DEFAULT: '#193579',
-        dark: '#009eeb',
-      },
-      lightblue: {
-        light: '#1C3E7E',
-        DEFAULT: '#1C3E7E',
-        dark: '#009eeb',
-      },
-    }
-  },
   plugins: [
     require('daisyui'),
   ],
   daisyui: {
-    styled: false,
+    styled: true,
+    themes: [
+    'light',
+    'dark',
+    'cupcake',
+    'bumblebee', 
+    'emerald', 
+    'corporate', 
+    'synthwave', 
+    'retro', 
+    'cyberpunk', 
+    'valentine', 
+    'halloween', 
+    'garden', 
+    'forest', 
+    'aqua', 
+    'lofi', 
+    'pastel', 
+    'fantasy', 
+    'wireframe', 
+    'black', 
+    'luxury', 
+    'dracula', 
+    'cmyk'
+      ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
   },
 }
