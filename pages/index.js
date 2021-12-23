e06ffe4689db2f6780255d9684def61df492b4f9
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-12-23 18:22:23 +0300

    feat(home): Made a very nice (imo) home page :)

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,37 @@
-import React, { useEffect, useState } from "react";
+import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
 
-function App() {
-  const calculateTimeLeft = () => {
-    let year = new Date().getFullYear();
-    const difference = +new Date(`${year}-9-1`) - +new Date();
-    let timeLeft = {};
-
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    }
-
-    return timeLeft;
-  };
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-  const [year] = useState(new Date().getFullYear());
-
-  useEffect(() => {
-    setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-  });
-
-  const timerComponents = [];
-
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
-
-    timerComponents.push(
-      <span>
-        {timeLeft[interval]} {interval}{" "}
-      </span>
-    );
-  });
+export default function Home() {
+  let pages = ["zejna", "nur", "hamza", "esma", "halima", "tahira", "haris"]
+  {/* TODO: Add more background images */}
+  let background = ["https://hamy.file.glass/DEAIt1Z42x.jpg", "https://hamy.file.glass/4orK9iAjXE.jpg"] 
+  function randomnumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  let x = randomnumber(0, 6)
+  let y = randomnumber(0, 1)
+  let randompage = "/family/" + pages[x]
+  let randombg = background[y]
   return (
-    <div className=" font-sans text-lg h-full w-full text-gray-900 text-center overflow-hidden items-center">
-      <h1>This website is under contstruction!</h1>
-      <h2>Come back in</h2>
-      <div>
-        {timerComponents.length ? timerComponents : <span>Time is up!</span>}   
+    <div>
+      <div className="hero min-h-screen backdrop-blur" style={{backgroundImage: `url(${randombg})`}}> 
+        <div className="hero-overlay bg-opacity-60"></div> 
+        <div className="text-center hero-content text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">
+                  Hello there
+                </h1> 
+            <p className="mb-5">
+                  Welcome to our website, here you can find a different page for each family member explaining who we are and what we do. Why not click this button and see random page!
+                </p> 
+              
+            <div className="btn btn-primary">
+            <Link href={`${randompage}`}>See a random page</Link>
+            </div>
+          </div>
+        </div>
       </div>
-
     </div>
-  );
+  )
 }
-
-export default App;
